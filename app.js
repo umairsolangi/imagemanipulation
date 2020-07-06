e5817e2ab55bc3520873fc05cdf1b64dc1230c84
umairsolangi <51870556+umairsolangi@users.noreply.github.com>
2020-07-06 14:55:11 +0500

    Image Manipulation JavaScript
    Image Manipulation JavaScript

---
+++ app.js
@@ -0,0 +1,32 @@
+var pop = alert("Click On Every Image & Enjoy")
+document.write(pop)
+
+function hide(){
+    var img = document.getElementById("magic")
+    img.className = "hidden"
+}
+
+function hide2(){
+    var img = document.getElementById("magic2")
+    img.className = "hidden"
+}
+
+function hide3(){
+    var img = document.getElementById("magic3")
+    img.className = "hidden"
+}
+
+function hide4(){
+    var img = document.getElementById("magic4")
+    img.className = "hidden"
+}
+
+function hide5(){
+    var img = document.getElementById("magic5")
+    img.className = "hidden"
+}
+
+function hide6(){
+    var img = document.getElementById("magic6")
+    img.className = "hidden"
+}
